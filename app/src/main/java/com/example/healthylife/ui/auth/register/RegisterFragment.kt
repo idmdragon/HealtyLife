6bf5d78c2c08d9b0ffca508e5eccb7c18908117f
pypoh <nafifb@gmail.com>
2020-07-15 23:37:31 +0700

    Auth v0.1

--- app/src/main/java/com/example/healthylife/ui/auth/register/RegisterFragment.kt
+++ app/src/main/java/com/example/healthylife/ui/auth/register/RegisterFragment.kt
@@ -1,24 +1,17 @@
-package com.example.healthylife
+package com.example.healthylife.ui.auth.register
 
-import android.content.ContentValues.TAG
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
+import com.example.healthylife.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.fragment_login.*
-import kotlinx.android.synthetic.main.fragment_sign_up.*
-import kotlinx.android.synthetic.main.fragment_sign_up.et_email
 
 
-class SignUpFragment : Fragment() {
+class RegisterFragment : Fragment() {
     private lateinit var auth: FirebaseAuth
 
 
